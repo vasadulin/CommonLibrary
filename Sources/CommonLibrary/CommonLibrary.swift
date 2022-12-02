861bb9a0276421b76771fac36507a9c7cab3cb61
Asadulin Viktor <vasadulin@gmail.com>
2022-12-02 19:44:38 +0200

    Initial Commit

---
+++ Sources/CommonLibrary/CommonLibrary.swift
@@ -0,0 +1,6 @@
+public struct CommonLibrary {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
---
+++ Tests/CommonLibraryTests/CommonLibraryTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import CommonLibrary
+
+final class CommonLibraryTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(CommonLibrary().text, "Hello, World!")
+    }
+}
